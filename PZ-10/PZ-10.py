85a0f7feb99222d6f2e483af963e99187e5866e4
Max Kasyanov <98693514+Dotingo@users.noreply.github.com>
2022-12-15 21:30:29 +0300

    Update PZ-10.py

--- PZ-10/PZ-10.py
+++ PZ-10/PZ-10.py
@@ -7,13 +7,32 @@
 
 
 def program():
-    voyage = {'Мексика', 'Канада', 'Израиль', 'Италия', 'США'}
-    reinaTour = {'Англия', 'Япония', 'Канада', 'ЮАР'}
-    rainbow = {'США', 'Испания', 'Швеция', 'Австралия'}
-    if 'США' in voyage or reinaTour or rainbow:
+    voyage = {
+        'name': 'voyage',
+        'country': {'Мексика', 'Канада', 'Израиль', 'Италия', 'США'},
+    }
+    reinaTour = {
+        'name': 'reinaTour',
+        'country': {'Англия', 'Япония', 'Канада', 'ЮАР'},
+    }
+    rainbow = {
+        'name': 'rainbow',
+        'country': {'США', 'Испания', 'Швеция', 'Австралия'},
+    }
+    allTour = [voyage, reinaTour, rainbow]
+    findUsa = []
+    findCanada = []
 
+    fullList = voyage['country'] | reinaTour['country'] | rainbow['country']
 
+    for tour in allTour:
+        if 'США' in tour['country']:
+            findUsa.append(tour['name'])
 
+    for tour in allTour:
+        if 'Канада' in tour['country']:
+            findCanada.append(tour['name'])
 
+    print(f"Тур в США есть в данных турах: {findUsa}\n Тур в Канаду есть в данных турах: {findCanada}")
 
 program()
